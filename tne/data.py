a103fc5b9ca0f2ccdb953fad7584e222e53410a8
Benjamin Wild <b.w@fu-berlin.de>
2020-02-20 12:06:48 +0100

    Add more data handling utility functions

--- tne/data.py
+++ tne/data.py
@@ -42,8 +42,14 @@ def parse_interaction_events(data_path, first_day_date, from_date_incl, to_date_
     return df
 
 
-def filter_alive_bees(df, alive_path, date):
+def load_alive_df(alive_path):
     alive_df = pd.read_csv(alive_path, parse_dates=['annotated_tagged_date', 'inferred_death_date'])
+
+    return alive_df
+
+
+def filter_alive_bees(df, alive_path, date):
+    alive_df = load_alive_df(alive_path)
 
     daily_alive = ((alive_df.annotated_tagged_date <= date) & (alive_df.inferred_death_date > date)).values
     bee_ids = alive_df[daily_alive].bee_id
@@ -66,4 +72,19 @@ def map_bee_ids(df, bee_ids):
     return df, id_to_idx, idx_to_id
 
 
+def subsample_individuals(df, num_individuals):
+    ids, counts = np.unique(df.bee_id_0, return_counts=True)
+    p = counts.astype(np.float) / np.sum(counts)
+    bee_ids = np.random.choice(ids, size=num_individuals, replace=False, p=p)
+    df = df[df.bee_id_0.isin(bee_ids) & df.bee_id_1.isin(bee_ids)]
 
+    return df, bee_ids
+
+
+def get_ages(date, alive_path, bee_ids):
+    alive_df = load_alive_df(alive_path)
+
+    tagged_dates = alive_df[alive_df.bee_id.isin(bee_ids)].sort_values('bee_id').annotated_tagged_date
+    ages_in_days = (date - tagged_dates).apply(lambda dt: dt.days).values
+
+    return ages_in_days
